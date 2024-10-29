006c12dfd87a2503056d7bb98cfc95800cd885e5
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-10-29 17:22:55 +0500

    Repository Pattern
    Repository Pattern implementation on storage

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "time"
+	"log"
+	"net/http"
+	"time"
 
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"github.com/alphadev97/gophersocial97/internal/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
     config config
+    store store.Storage
 }
 
 type config struct {
