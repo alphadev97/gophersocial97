44d6314acf706cd444ef2b8b027c39db630e9b0e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-12-12 00:47:34 +0500

    Configuring the DB Connection Pool
    Configuring the DB Connection Pool TablePlus

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/alphadev97/gophersocial97/internal/db"
 	"github.com/alphadev97/gophersocial97/internal/env"
 	"github.com/alphadev97/gophersocial97/internal/store"
 )
@@ -11,9 +12,30 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:        env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConn: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConn: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConn,
+		cfg.db.maxIdleConn,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+  defer db.Close()
+
+  log.Println("Database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
