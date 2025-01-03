e3301903c2964c8fe64e64738416a290a509eb54
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2025-01-04 00:55:09 +0500

    Delete Post By ID

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -77,3 +77,23 @@ func (s *PostsStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 
 	return &post, nil
 }
+
+func (s *PostsStore) Delete(ctx context.Context, postID int64) error {
+	query := `DELETE FROM posts WHERE id = $1`
+
+	res, err := s.db.ExecContext(ctx, query, postID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+}
